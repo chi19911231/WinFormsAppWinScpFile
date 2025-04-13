ae14f144cb0e6fe10936c591e7e29ad9107c4582
陳廣旂 <chi_19911231@outlook.com>
2025-04-14 00:20:22 +0800

    新增appsettings相關設定

--- WinFormsAppBase/Settings/AppSettings.cs
+++ WinFormsAppBase/Settings/AppSettings.cs
@@ -8,19 +8,33 @@
         /// <summary>
         /// 程式名稱
         /// </summary>
-        public string AppName { get; set; }
+        public string? AppName { get; set; }
 
         /// <summary>
-        /// 
+        /// 下載IP、下載網域名稱
         /// </summary>
-        public string ApiUrl { get; set; }
+        public string? DownloadHostName { get; set; }
         /// <summary>
-        /// 
+        /// 下載使用者名稱
         /// </summary>
-        public int RetryCount { get; set; }
+        public string? DownloadUserName { get; set; }
         /// <summary>
-        /// 
+        /// 下載密碼
         /// </summary>
-        public bool FeatureEnabled { get; set; }
+        public string? DownloadPassword { get; set; }
+
+        /// <summary>
+        /// 上傳IP、上傳網域名稱
+        /// </summary>
+        public string? UploadHostName { get; set; }
+        /// <summary>
+        /// 上傳使用者名稱
+        /// </summary>
+        public string? UploadUserName { get; set; }
+        /// <summary>
+        /// 上傳密碼
+        /// </summary>
+        public string? UploadPassword { get; set; }
+ 
     }
 }
